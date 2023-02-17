dea6d91f3bff96e15150e35c79a62aacfe28b24a
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-02-16 19:14:09 -0800

    Hello, printf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program on size is not grandeur, and territory does not make a nation
+ * this means it prints the size of various computer type
+ * Return 0
+ */
+int main(void)
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("Size of a char: %lu bytes\n", (unsigned long) sizeof(a));
+printf("Size of a int: ");
+printf("Size of a long int: ");
+printf("SIze of a long long int: ");
+printf("Size of a float: ");
+return (0);
+}
