fbcf8b45a78f1a30327b6f34ba1ef803b670bb6e
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-02-17 09:13:18 -0800

    Task 6, Size of different computer types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("Size of a char: %zu bytes\n", sizeof(char));
-printf("Size of a int: %zu bytes\n", sizeof(int));
-printf("Size of a long int: %zu bytes\n", sizeof(long int));
-printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
-printf("Size of a float: %zu bytes\n", sizeof(float));
+printf("Size of a char: %zu bytes\n", sizeof(a));
+printf("Size of a int: %zu bytes\n", sizeof(b));
+printf("Size of a long int: %zu bytes\n", sizeof(c));
+printf("Size of a long long int: %zu bytes\n", sizeof(d));
+printf("Size of a float: %zu bytes\n", sizeof(f));
 return (0);
 }
