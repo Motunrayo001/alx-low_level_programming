31fdac504f5bd544758759a4748b245e8b58e1f8
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-02-16 17:41:02 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - A program to print without using the printf but puts funtion
+ * Return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
