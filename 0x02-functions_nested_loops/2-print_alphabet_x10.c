c95ebf3eb07b7456df21c42aa78b56ded730fa8d
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-02-22 17:42:31 -0800

    A function that prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,20 @@
  * main- make alphabet x10 times
  * Return: 0
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char c;
-	int i = 0;
+	int i;
+
+	i = 0;
 
 	while (i <= 9)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		c = 'a';
+		while (c <= 'z')
 		{
 			_putchar(c);
+			c++;
 		}
 		_putchar('\n');
 		i++;
