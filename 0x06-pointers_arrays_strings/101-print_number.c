0642e3c2833436c7f2358cd226988244a9ae3e56
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-03-02 04:52:44 -0800

    Print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int y;
+
+	y = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		y = -n;
+	}
+	if (y / 10 != 0)
+	{
+		print_number(y / 10);
+	}
+	_putchar((y % 10) + '0');
+}
