412b6131276b01686e1b44880cfb4be8460419e8
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-03-03 15:35:17 -0800

    Capitalize all first strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *str)
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
-				str[index - 1] == '.' ||
+				str[index - 1] == ',' ||
 				str[index - 1] == ';' ||
 				str[index - 1] == '.' ||
 				str[index - 1] == '!' ||
@@ -24,13 +24,13 @@ char *cap_string(char *str)
 				str[index - 1] == '"' ||
 				str[index - 1] == '(' ||
 				str[index - 1] == ')' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
 				index == 0)
 		{
 			str[index] -= 32;
-			index++;
 		}
+			index++;
 	}
 	return (str);
 }
