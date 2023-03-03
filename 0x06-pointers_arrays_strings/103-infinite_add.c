2872d5a8687418a636aeb27b9ea15815d5375fd6
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-03-03 04:55:45 -0800

    Magic

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *infinite_add- function that adds two numbersfunction that adds two numbers
+ * @n1: the first number
+ * @n2: the second number
+ * @size_r:  buffer size
+ * @r: buffer that the function will use to store the result
+ * You can assume that you will always get positive numbers, or 0
+ * You can assume that there will be only digits in the strings n1 and n2
+ * n1 and n2 will never be empty
+ * Return: pointer to the result
+ * If the result can not be stored in r the function must return 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
