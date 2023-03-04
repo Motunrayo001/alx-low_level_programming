d03c133023e77fc3b1c12e8ba6c123b237969c39
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-03-03 16:21:32 -0800

    Print an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,17 +7,17 @@
  */
 void print_number(int n)
 {
-	unsigned int y;
+	unsigned int n1;
 
-	y = n;
+	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		y = -n;
+		n1 = -n;
 	}
-	if (y / 10 != 0)
+	if (n1 / 10 != 0)
 	{
-		print_number(y / 10);
+		print_number(n1 / 10);
 	}
-	_putchar((y % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
