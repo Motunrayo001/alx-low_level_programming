82e47d6fcd0a3ff4f7050e35ab83c72680eb8f4f
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-03-16 20:51:04 -0700

    We must accept finite disappointment, but never lose to infinte hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -19,27 +19,86 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, d1, d2, *result, a = 0;
 
-	if (argc != 3)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		for (i = 0; i <= len1 + len2; i++)
+			result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		d1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			d2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (d1 * d2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	mul = atoi(argv[1]) * atoi(argv[2]);
-	printf("%lu\n", mul);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
 
+/**
+ * is_digit - check if there is non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if there is non-digit, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - return the length of a string
+ * @s: The string to get the length
+ * Return: length of @s
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handling errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
