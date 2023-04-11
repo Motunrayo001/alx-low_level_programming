8e44561d1a6542e56a42c20c9360e3cae8560acf
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-04-11 16:24:30 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,20 +9,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int a, c = 0;
-	unsigned long int b;
-
-	for (a = 63; a >= 0; a--)
-	{
-		b = n >> 1;
-		if (b & a)
-		{
-			_putchar('1');
-			c++;
-		}
-		else if (c)
-			_putchar('0');
-	}
-	if (!c)
-		_putchar('0');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
