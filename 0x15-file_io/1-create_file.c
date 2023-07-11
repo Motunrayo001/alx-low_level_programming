86b031ddc3c08a6f183f608eb160c94e5c1c58b7
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-07-11 03:28:32 +0100

    ALX Project on 0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 			l++;
 	}
 	wrt = write(f, text_content, l);
-	if (f == -1 || w == -1)
+	if (f == -1 || wrt == -1)
 		return (-1);
 	close(f);
 	return (1);
