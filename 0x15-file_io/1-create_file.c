9a3ca9b59d1160d6b65821a09fbf4a355d2b7fe5
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-07-11 03:35:11 +0100

    Project 0x15-file_io files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,14 +19,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-	{
-		f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	}
+		return (-1);
 	else
 	{
 		for (l = 0; text_content[l];)
 			l++;
 	}
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(f, text_content, l);
 	if (f == -1 || wrt == -1)
 		return (-1);
