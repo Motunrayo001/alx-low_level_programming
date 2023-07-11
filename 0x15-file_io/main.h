f43ce080b5fa3f63ee03437a3be203c0aff38603
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-07-11 02:30:02 +0100

    ALX Project on 0x15-file_io

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+void close_file(int fd_value);
+char *create_buffer(char *fb);
+char *read_buffer(char *file);
+
+#endif
