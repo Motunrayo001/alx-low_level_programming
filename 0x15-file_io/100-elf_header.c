2161a468ce1585c058e371642ba3233b51f3d8c2
Motunrayo <motunrayo.ogunrinde@gmail.com>
2023-07-13 09:11:48 -0700

    Project 0x15-file_io files

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -25,3 +25,68 @@
  * You are allowed to use printf
  * Return: nothing
  */
+
+/**
+ * check_elf - first check if the file is elf
+ * @e_ident: pointer to an array that has elf magic numbers
+ * if the file is not an elf file,  exit with status code 98
+ * and display a comprehensive error message to stderr
+ */
+void check_elf(unsigned char *e_ident)
+{
+	int i;
+
+	for (i = 0; i < 4; i++)
+	{
+		if (e_ident[i] != 127 &&
+				e_ident[i] != 'E' &&
+				e_ident[i] != 'L' &&
+				e_ident[i] != 'F')
+		{
+			dprintf(STDERR_FILENO: "Error: It's not an ELF file\n");
+			exit(98);
+		}
+	}
+}
+
+/**
+ * magic_elf - print the magic numbers of the ELF file header
+ * @e_ident: pointer to an array that has elf magic numbers
+ */
+void magic_elf(unsigned char *e_ident)
+{
+	int i;
+
+	printf(" Magic: ");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x", e_ident[i]);
+		if (i != EI_NIDENT - 1)
+			printf(" ")
+		else
+			printf("\n")
+	}
+}
+
+/**
+ * elf_class - the class of an ELF header file
+ * @e_ident: pointer to an array that has elf class
+ */
+void elf_class(unsigned char *e_ident)
+{
+	printf(" Class: ");
+	switch (e_ident[EI_CLASS])
+	{
+		case ELFCLASSNONE:
+			printf("none\n");
+			break;
+		case ELFCLASS32:
+			printf("ELF32\n");
+			break;
+		case ELFCLASS64:
+			printf("ELF64\n");
+			break;
+		default:
+			printf("unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
